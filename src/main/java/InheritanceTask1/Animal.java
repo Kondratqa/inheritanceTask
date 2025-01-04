25eff3f20f962d9c7a66622a704363ffd5e49409
kinomagnum <kinomagnum@gmail.com>
2025-01-04 16:12:50 +0300

    Animal3

--- src/main/java/InheritanceTask1/Animal.java
+++ src/main/java/InheritanceTask1/Animal.java
@@ -4,16 +4,25 @@ public class Animal {
 
     private String name;
 
+
     public Animal(String name) {
         this.name = name;
+
     }
 
     public String getName() {
         return name;
     }
 
-    public  void makeSound() {
-        System.out.println("Лошадь ржет");
+
+    public void makeSound() {
+        if (this.name.equals("hours")){
+            System.out.println("Лошадь ржет");
+        }else if (this.name.equals("cat")){
+            System.out.println("Кот мяукает");
+        }
     }
 }
+
+
 
--- src/main/java/InheritanceTask1/Main.java
+++ src/main/java/InheritanceTask1/Main.java
@@ -2,9 +2,12 @@ package InheritanceTask1;
 
 public class Main {
     public static void main(String[] args) {
-        Animal Hours = new Animal("Hours");
-        System.out.println(Hours.getName());
-        Hours.makeSound();
+        Animal hours = new Animal("hours");
+        System.out.println(hours.getName());
+        hours.makeSound();
+
+        Animal cat = new Animal("cat");
+        cat.makeSound();
     }
 }
 
